c44edda683ad3dd5527ead0c5b8f6e98e73081a7
Rulbed <pathrulbed@gmail.com>
2023-04-26 19:58:30 +0700

    ทำได้แล้ว

--- Program.cs
+++ Program.cs
@@ -1,91 +1,92 @@
 ﻿class Program
 {
-    static void Stall_Reseved(bool x)
-    {
-        if (x==true)
-        {
-            Console.WriteLine("The stall is reserved.");
-        }
-    }
-     static void Stall_All(bool x)
-    {
-        if (x==true)
-        {
-            Console.WriteLine("All stall are reserved.");
-        }
-    }
     static void Main(string[] args)
     {
         Console.WriteLine("Plesae Input Number Of Stall");
         int Num_Stall = int.Parse(Console.ReadLine());
         char [] Stall = new char [Num_Stall];
-        int [] Input_Num = new int [2];
-        bool StallIsReseved = false;
-        bool StallIsAll = false;
+        int Stop = 0;
         for (int i = 0;i<Num_Stall;i++)
         {
             Stall[i] = '_';
         }
-        Console.WriteLine(Stall);
-        int Stall_Break = 0;
-        
-        while (true)
+        while(Stop<Num_Stall-1)
         {
-            for (int i = 0;i<2;i++)
+            Console.WriteLine("Plesae Input Location Of Stall");
+            int Location_A = int.Parse(Console.ReadLine());
+            int Location_B = int.Parse(Console.ReadLine());
+            if ((Location_A<1)&&(Location_B<1))
             {
-                Console.WriteLine("Plesae Input Location Of Stall");
-                int Stall_Position = int.Parse(Console.ReadLine());
-                if (Stall_Position<=Num_Stall)
-                {
-                    Input_Num[i] = Stall_Position;
-                }
-                
+                Stop=Num_Stall;
             }
 
-            Console.WriteLine(Input_Num);
-            
-            for (int i = 0;i<Num_Stall;i++)
+            else
+            {
+                if (Stop<Num_Stall)
+                {
+                    if (Location_A==Location_B)
                     {
-                        if (Stall[i]=='X')
-                        {
-                            Stall_Break++;
-                        }
-
-                        if (Stall_Break>=Num_Stall)
-                        {
-                            Stall_All(true);
-                            StallIsAll = true;
-                            break;
-                        }
+                        Stall[Location_A-1] = 'X';
+                        Stop++;
                     }
-            
-            for (int i = 0;i<2;i++)
-            {
-                if (Stall[Input_Num[i]-1]=='X')
+                    else
+                    {
+                      if ((Location_A>0)&&(Location_B>0))
+                      {
+                        if (Stop+2>=Num_Stall)
+                        {
+                            Console.WriteLine("The entrance can't be reserved.");
+                        }
+                        else if ((Stall[Location_A-1]=='X')||(Stall[Location_B-1]=='X'))
+                        {
+                            Console.WriteLine("The stall is reserved.");
+                        }
+                        else
+                        {
+                            Stall[Location_A-1] = 'X';
+                            Stall[Location_B-1] = 'X';
+                            Stop = Stop+2;
+                        }
+                        
+                      }
+                      else
+                      {
+                        if (Location_A>0)
+                        {
+                            if (Stall[Location_A-1]=='_')
+                            {
+                                Stall[Location_A-1] = 'X';
+                                Stop++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("The stall is reserved.");
+                            }
+                        }
+                        if (Location_B>0)
+                        {
+                            if (Stall[Location_B-1]=='_')
+                            {
+                                Stall[Location_B-1] = 'X';
+                                Stop++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("The stall is reserved.");
+                            }
+                        }
+                      }
+
+                    }
+                }
+                else
                 {
-                    Stall_Reseved(true);
-                    StallIsReseved = true;
-                    break;
+                    Console.WriteLine("All stall are reserved.");
                 }
             }
-
-            if ((StallIsAll==false)&&(StallIsReseved==false))
-            {
-                for (int i = 0;i<2;i++)
-                {
-                   if (Input_Num[i]>0)
-                   {
-                    Stall[Input_Num[i]-1] = 'X';
-                   }
-                }
-            }
-
-
-
-
-
-            
             Console.WriteLine(Stall);
+            
         }
+        
     }
 }
